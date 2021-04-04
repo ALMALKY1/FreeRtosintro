69ab2fd6d373eff342bc7a9f71b9c2fb3d17c1e8
Mohamed Almalky <MohamedELMALKY0@gmail.com>
2021-04-04 16:45:45 +0200

    Task

--- TASK2.C
+++ TASK2.C
@@ -1,124 +1,222 @@
-;/*
-; * FreeRTOS Kernel V10.4.3
-; * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
-; *
-; * Permission is hereby granted, free of charge, to any person obtaining a copy of
-; * this software and associated documentation files (the "Software"), to deal in
-; * the Software without restriction, including without limitation the rights to
-; * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-; * the Software, and to permit persons to whom the Software is furnished to do so,
-; * subject to the following conditions:
-; *
-; * The above copyright notice and this permission notice shall be included in all
-; * copies or substantial portions of the Software.
-; *
-; * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-; * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-; * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-; * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-; * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-; * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-; *
-; * https://www.FreeRTOS.org
-; * https://github.com/FreeRTOS
-; *
-; * 1 tab == 4 spaces!
-; */
+/*
+ * FreeRTOS Kernel V10.2.0
+ * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://www.FreeRTOS.org
+ * http://aws.amazon.com/freertos
+ *
+ * 1 tab == 4 spaces!
+ */
 
-	INCLUDE portmacro.inc
-
-	IMPORT	vTaskSwitchContext
-	IMPORT	xTaskIncrementTick
-
-	EXPORT	vPortYieldProcessor
-	EXPORT	vPortStartFirstTask
-	EXPORT	vPreemptiveTick
-	EXPORT	vPortYield
+/* 
+	NOTE : Tasks run in system mode and the scheduler runs in Supervisor mode.
+	The processor MUST be in supervisor mode when vTaskStartScheduler is 
+	called.  The demo applications included in the FreeRTOS.org download switch
+	to supervisor mode prior to main being called.  If you are not using one of
+	these demo application projects then ensure Supervisor mode is used.
+*/
 
 
-VICVECTADDR	EQU	0xFFFFF030
-T0IR		EQU	0xE0004000
-T0MATCHBIT	EQU	0x00000001
+/*
+ * Creates all the demo application tasks, then starts the scheduler.  The WEB
+ * documentation provides more details of the demo application tasks.
+ * 
+ * Main.c also creates a task called "Check".  This only executes every three 
+ * seconds but has the highest priority so is guaranteed to get processor time.  
+ * Its main function is to check that all the other tasks are still operational.
+ * Each task (other than the "flash" tasks) maintains a unique count that is 
+ * incremented each time the task successfully completes its function.  Should 
+ * any error occur within such a task the count is permanently halted.  The 
+ * check task inspects the count of each task to ensure it has changed since
+ * the last time the check task executed.  If all the count variables have 
+ * changed all the tasks are still executing error free, and the check task
+ * toggles the onboard LED.  Should any task contain an error at any time 
+ * the LED toggle rate will change from 3 seconds to 500ms.
+ *
+ */
 
-	ARM
-	AREA	PORT_ASM, CODE, READONLY
+/* Standard includes. */
+#include <stdlib.h>
+#include <stdio.h>
 
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "lpc21xx.h"
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Starting the first task is done by just restoring the context
-; setup by pxPortInitialiseStack
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-vPortStartFirstTask
-
-	PRESERVE8
-
-	portRESTORE_CONTEXT
-
-vPortYield
-
-	PRESERVE8
-
-	SVC 0
-	bx lr
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Interrupt service routine for the SWI interrupt.  The vector table is
-; configured in the startup.s file.
-;
-; vPortYieldProcessor() is used to manually force a context switch.  The
-; SWI interrupt is generated by a call to taskYIELD() or portYIELD().
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-vPortYieldProcessor
-
-	PRESERVE8
-
-	; Within an IRQ ISR the link register has an offset from the true return
-	; address, but an SWI ISR does not.  Add the offset manually so the same
-	; ISR return code can be used in both cases.
-	ADD	LR, LR, #4
-
-	; Perform the context switch.
-	portSAVE_CONTEXT					; Save current task context
-	LDR R0, =vTaskSwitchContext			; Get the address of the context switch function
-	MOV LR, PC							; Store the return address
-	BX	R0								; Call the contedxt switch function
-	portRESTORE_CONTEXT					; restore the context of the selected task
+/* Peripheral includes. */
+#include "serial.h"
+#include "GPIO.h"
 
 
+/*-----------------------------------------------------------*/
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Interrupt service routine for preemptive scheduler tick timer
-; Only used if portUSE_PREEMPTION is set to 1 in portmacro.h
-;
-; Uses timer 0 of LPC21XX Family
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+TaskHandle_t LED_ONE_Handler  = NULL;
+TaskHandle_t LED_TWO_Handler  = NULL;
+TaskHandle_t LED_THREE_Handler = NULL;
 
-vPreemptiveTick
+pinState_t Button_state ; 
 
-	PRESERVE8
+/* Constants to setup I/O and processor. */
+#define mainBUS_CLK_FULL	( ( unsigned char ) 0x01 )
 
-	portSAVE_CONTEXT					; Save the context of the current task.
+/* Constants for the ComTest demo application tasks. */
+#define mainCOM_TEST_BAUD_RATE	( ( unsigned long ) 115200 )
 
-	LDR R0, =xTaskIncrementTick			; Increment the tick count.
-	MOV LR, PC							; This may make a delayed task ready
-	BX R0								; to run.
 
-	CMP R0, #0
-	BEQ SkipContextSwitch
-	LDR R0, =vTaskSwitchContext			; Find the highest priority task that
-	MOV LR, PC							; is ready to run.
-	BX R0
-SkipContextSwitch
-	MOV R0, #T0MATCHBIT					; Clear the timer event
-	LDR R1, =T0IR
-	STR R0, [R1]
+/*
+ * Configure the processor for use with the Keil demo board.  This is very
+ * minimal as most of the setup is managed by the settings in the project
+ * file.
+ */
+static void prvSetupHardware( void );
+/*-----------------------------------------------------------*/
 
-	LDR	R0, =VICVECTADDR				; Acknowledge the interrupt
-	STR	R0,[R0]
 
-	portRESTORE_CONTEXT					; Restore the context of the highest
-										; priority task that is ready to run.
-	END
+/* Task to be created. */
+void vTask_LED_ONE( void * pvParameters )
+{
+	
+    for( ;; )
+    {
+			
+				GPIO_write(PORT_0 , PIN1 , PIN_IS_HIGH ) ; 
+			vTaskDelay(100);
+				GPIO_write(PORT_0, PIN1 , PIN_IS_LOW) ;
+			vTaskDelay(100);
+				
+    }
+		
+		
+
+	}
+
+
+/* Task to be created. */
+void vTask_LED_TWO( void * pvParameters )
+{
+	
+    for( ;; )
+    {
+			
+				GPIO_write(PORT_0 , PIN2 , PIN_IS_HIGH ) ; 
+			vTaskDelay(500);
+				GPIO_write(PORT_0, PIN2 , PIN_IS_LOW) ;
+			vTaskDelay(500);
+				
+    }
+		
+		
+
+	}
+
+	
+	/* Task to be created. */
+void vTask_LED_THREE( void * pvParameters )
+{
+	
+    for( ;; )
+    {
+			
+				GPIO_write(PORT_0 , PIN3 , PIN_IS_HIGH ) ; 
+			vTaskDelay(1000);
+				GPIO_write(PORT_0, PIN3 , PIN_IS_LOW) ;
+			vTaskDelay(1000);
+				
+    }
+		
+		
+
+	}
+
+/*
+ * Application entry point:
+ * Starts all the other tasks, then starts the scheduler. 
+ */
+int main( void )
+{
+	/* Setup the hardware for use with the Keil demo board. */
+	prvSetupHardware();
+
+	
+    /* Create Tasks here */
+
+	
+    /* Create the task, storing the handle. */
+     xTaskCreate(
+                    vTask_LED_ONE,       /* Function that implements the task. */
+                    "LED1_Task",          /* Text name for the task. */
+                    100,      /* Stack size in words, not bytes. */
+                    ( void * ) 0,    /* Parameter passed into the task. */
+                    1,/* Priority at which the task is created. */
+                    & LED_ONE_Handler );      /* Used to pass out the created task's handle. */
+
+	
+		  xTaskCreate(
+                    vTask_LED_TWO,       /* Function that implements the task. */
+                    "LED2_Task",          /* Text name for the task. */
+                    100,      /* Stack size in words, not bytes. */
+                    ( void * ) 0,    /* Parameter passed into the task. */
+                    1,/* Priority at which the task is created. */
+                    & LED_TWO_Handler );      /* Used to pass out the created task's handle. */
+
+	
+		  xTaskCreate(
+                    vTask_LED_THREE,       /* Function that implements the task. */
+                    "LED3_Task",          /* Text name for the task. */
+                    100,      /* Stack size in words, not bytes. */
+                    ( void * ) 0,    /* Parameter passed into the task. */
+                    1,/* Priority at which the task is created. */
+                    & LED_THREE_Handler );      /* Used to pass out the created task's handle. */
+
+	
+
+	/* Now all the tasks have been started - start the scheduler.
+
+	NOTE : Tasks run in system mode and the scheduler runs in Supervisor mode.
+	The processor MUST be in supervisor mode when vTaskStartScheduler is 
+	called.  The demo applications included in the FreeRTOS.org download switch
+	to supervisor mode prior to main being called.  If you are not using one of
+	these demo application projects then ensure Supervisor mode is used here. */
+	vTaskStartScheduler();
+
+	/* Should never reach here!  If you do then there was not enough heap
+	available for the idle task to be created. */
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+static void prvSetupHardware( void )
+{
+	/* Perform the hardware setup required.  This is minimal as most of the
+	setup is managed by the settings in the project file. */
+
+	/* Configure UART */
+	xSerialPortInitMinimal(mainCOM_TEST_BAUD_RATE);
+
+	/* Configure GPIO */
+	GPIO_init();
+
+	/* Setup the peripheral bus to be the same as the PLL output. */
+	VPBDIV = mainBUS_CLK_FULL;
+}
+/*-----------------------------------------------------------*/
+
 
